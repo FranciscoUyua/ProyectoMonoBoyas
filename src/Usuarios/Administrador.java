842bf1c8fd45a9c38fc55c5dea3a88df6ca93e55
Nach <152998765+NachoIommi@users.noreply.github.com>
2026-06-19 19:47:45 -0300

    Administrador Pseudo

--- src/Usuarios/Administrador.java
+++ src/Usuarios/Administrador.java
@@ -1,33 +1,35 @@
 package Usuarios;
-
+import Usuarios.*;
 import Operaciones.*;
+import Persistencia.UsuarioAlertaDAO;
 import Equipamiento.*;
-import java.util.ArrayList;
-import java.util.List;
-
-import Alertas.Alerta;
 
 
 public class Administrador extends Usuario {
 
-    protected int id;
-    protected String nombre;
-    protected String contrasena;
-    protected int dni;
-    protected List<Alerta> alertasRecibidas; // Lista para almacenar las alertas recibidas por el usuario
-    protected String rol;
+    protected OperadorBuque operadorBuque; // Referencia al operador de buque asignado
+    protected Usuario usuario; // Referencia al usuario que se está gestionando (para agregar, eliminar o modificar)
     protected Planta planta; // Referencia a la planta que opera
 
     public Administrador(int id, String nombre, String contrasena, int dni) {
         super(id, nombre, contrasena, dni);
-        this.alertasRecibidas = new ArrayList<>();
         rol = "ADMIN";
     }
 
     public String getRol() {
     return rol;
     }
+    public void agregarUsuario(Usuario nuevoUsuario) {
+        
+    }
 
+    public void eliminarUsuario(int idUsuario) {
+        
+    }
+
+    public void modificarUmbrales() {
+        
+    }
     // Métodos específicos para Administrador
     public void PlanificarOperacion(Buque barco,OperadorBuque operadorBuque) {
         Operacion operacion = new Operacion((int)(Math.random() * 100),  barco, operadorBuque); 
--- src/Usuarios/OperadorBuque.java
+++ src/Usuarios/OperadorBuque.java
@@ -1,9 +1,18 @@
 package Usuarios;
 
 import Alertas.Alerta;
+import Operaciones.Operacion;
+import Usuarios.*;
+    
+
 import java.util.ArrayList;
+import java.util.List;
 
 public class OperadorBuque extends UsuarioOperador {
+
+    protected List<Alerta> alertasRecibidas; // Lista para almacenar las alertas recibidas por el usuario
+    protected String rol;
+    protected Operacion operacion;
 
     public OperadorBuque(int id, String nombre, String contrasena, int dni) {
         super(id, nombre, contrasena, dni);
@@ -12,13 +21,16 @@ public class OperadorBuque extends UsuarioOperador {
     }
 
     public String getRol() {
-    return rol;
+        return rol;
     }
 
     @Override
     public void reconocerAlerta(Alerta alerta) {
-        // Esta lógica se ejecutará cuando el capitán haga clic en el botón "Aceptar/Reconocer" en su pantalla
-        System.out.println("  [✔] OperadorBuque (" + this.nombre + ") ha RECONOCIDO la alerta ID: " + alerta.getId());
-        // Aquí a futuro se podría cambiar el estado de la alerta: alerta.setEstado("RECONOCIDA");
+        System.out.println("\n[OPERADOR DE BUQUE] >>> ALERTA RECONOCIDA. Tomando medidas de seguridad.");
     }
+    public void recibirAlerta(Alerta alerta) {
+        alertasRecibidas.add(alerta);
+        System.out.println("\n[OPERADOR DE BUQUE] >>> ALERTA RECIBIDA: " + alerta.getMensaje() + " (Tipo: " + alerta.getTipo_Alerta() + ")");
+    }
+
 }
