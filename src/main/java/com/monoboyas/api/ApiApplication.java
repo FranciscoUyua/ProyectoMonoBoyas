2b53c55c42a4b5a29837eb980bcaf3a18cc1474d
Juanma <juanmacristobo@gmail.com>
2026-06-20 22:51:30 -0300

    activar scheduler

--- src/main/java/com/monoboyas/api/ApiApplication.java
+++ src/main/java/com/monoboyas/api/ApiApplication.java
@@ -3,8 +3,9 @@ package com.monoboyas.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-//@EnableScheduling
+@EnableScheduling
 @SpringBootApplication
 @ComponentScan("com.monoboyas")
 public class ApiApplication {
